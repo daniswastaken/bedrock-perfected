544cfce7850f1e4079a4f62650f446db8aa8c4f5
daniswastaken <daniswastaken@gmail.com>
2026-01-01 14:55:31 +0700

    v26.1 release

---
+++ Bedrock Perfected BP/scripts/main.js
@@ -0,0 +1,11 @@
+import registerMobNaming from "./fxname/namesMain.js";
+import coordinateCompass from "./coordinateCompass/coordinateCompassMain.js";
+import damageMain from "./damage/damageMain.js";
+
+import raycast from "./labs/raycast.js";
+
+registerMobNaming();
+coordinateCompass();
+damageMain();
+
+// raycast();
